84ce8b4f4c4172a730174478bf506740f4563805
Fatima Ezzahra Ouaourikt <fouaouri@c4r1p10.1337.ma>
2026-03-26 14:28:43 +0100

    I handle the errors

--- first_program.py
+++ first_program.py
@@ -1,27 +1,15 @@
-
-# The first program will be used to predict the price of a car for a given mileage.
-# When you launch the program, it should prompt you for a mileage, and then give
-# you back the estimated price for that mileage. The program will use the following
-# hypothesis to predict the price :
-
-#                estimateP rice(mileage) = θ0 + (θ1 ∗ mileage)
-
-# Before running the training program, theta0 and theta1 will be set to 0.
-
 import pandas as pd
 import sys
 import json
 
 
 def checkTheInput(mileage):
+    if not mileage:
+        print("Please enter a valid mileage.")
+        return False
     if mileage < 0:
         print("Mileage cannot be negative. Please enter a valid mileage.")
         return False
-    # try:
-    #     assert(all([m in '-0123456789' for m in mileage]))
-    # except:
-    #     print('\nPlease use a valid litteral value for int() with base 10.')
-    #     sys.exit(-1)
     return True
 
 def get_thetas():
@@ -43,7 +31,7 @@ def get_min_max():
         with open('min_max.json', 'r') as f:
             params = json.load(f)
             min_km = params.get('min_km', 0)
-            max_km = params.get('max_km', 1)  # Avoid division by zero
+            max_km = params.get('max_km', 1)
             return min_km, max_km
     except FileNotFoundError:
         print("Min and max values file not found. Using default values (min_km=0, max_km=1).")
@@ -54,20 +42,23 @@ def get_min_max():
 def predictPrice():
     theta0, theta1 = get_thetas()
     min_km, max_km = get_min_max()
-    print(f"Using model parameters: theta0={min_km}, theta1={max_km}")
     dataFrame = pd.read_csv('data.csv')
     km = dataFrame.drop('price', axis=1)
     price = dataFrame['price']
 
     while True:
         inputt = input("Enter the mileage: ")
-        if inputt == "exit":
+        if inputt == "":
+            print("There is no mileage value !")
+            continue
+        elif inputt == "exit":
             print("Exiting the program.")
             sys.exit(0)
         else:
+            if inputt.isalpha():
+                print("Please enter a valid mileage.")
+                continue
             mileage = float(inputt)
-            if not mileage:
-                print("Mileage cannot be empty. Please enter a valid mileage.")
             if not checkTheInput(mileage): 
                 continue
             normalizedKm = (mileage - min_km) / (max_km - min_km)
--- second_program.py
+++ second_program.py
@@ -1,8 +1,3 @@
-# The second program will be used to train your model. It will read your dataset file
-# and perform a linear regression on the data.
-# Once the linear regression has completed, you will save the variables theta0 and
-# theta1 for use in the first program.
-
 import pandas as pd
 import sys
 import json
@@ -38,6 +33,7 @@ def trainingData(dataFrame):
         json.dump({'theta0': float(theta0), 'theta1': float(theta1)}, f)
     with open("min_max.json", 'w') as f:
         json.dump({'min_km': float(min_km), 'max_km': float(max_km)}, f)
+
 if __name__ == "__main__":
     dataFrame = pd.read_csv(sys.argv[1])
     trainingData(dataFrame)
